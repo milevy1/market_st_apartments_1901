83c35690f2de6a7a137c901e6d08d487566f3637
Matt Levy <milevy1@gmail.com>
2019-02-06 09:15:44 -0700

    Added apartment class with initial attributes

---
+++ lib/apartment.rb
@@ -0,0 +1,16 @@
+class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
+  def initialize(attributes)
+    @number = attributes[:number]
+    @monthly_rent = attributes[:monthly_rent]
+    @bathrooms = attributes[:bathrooms]
+    @bedrooms = attributes[:bedrooms]
+    @renter = nil
+  end
+
+end
